cbca473271542899a0e512ab1022eca91af967a6
Mike Calvanese <mfcalvanese@gmail.com>
2018-03-17 10:55:50 -0400

    new kovan deploy!

--- src/contracts/Tokens.js
+++ src/contracts/Tokens.js
@@ -2,13 +2,13 @@ import castleImg from '../img/castle.png'
 import moogleImg from '../img/happy-moogle.png'
 
 export default {
-  "0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9": {
+  "0xdd0036f6112d08722636f327b84a3fc3118f85ee": {
     "name": "Castle",
     "img": castleImg,
     "bio": "Well hey there, I'm a European Castle! I may be old and dusty, but I have charm! Buy me today and impress all your friends!",
     "features": "Rainbow paint job, perfect symetry, random hot air balloon"
   },
-  "0xc4c7497fbe1a886841a195a5d622cd60053c1376": {
+  "0x839d02b7b272d6214ee480d47580df1ce6835961": {
     "name": "Moogle",
     "img": moogleImg,
     "bio": "Hi there! I’m a happy moogle who enjoys eating vines and kupo nuts. I spend most of my time playing basketball and currently looking for someone to play with me!",
