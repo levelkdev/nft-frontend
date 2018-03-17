74de5d22c4a9ae89e51da99796795420b811db9d
Gal Shenar <gshenar@gmail.com>
2018-03-16 21:44:11 -0400

    add the addtoken stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import AddToken from './AddToken'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <AddToken />,
+  <App />,
   document.getElementById('root')
 );
