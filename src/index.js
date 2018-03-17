b25e67295bfe18769b5cbe739d60c5a36f324952
Gal Shenar <gshenar@gmail.com>
2018-03-16 21:36:47 -0400

    add token page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import AddToken from './AddToken'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <App />,
+  <AddToken />,
   document.getElementById('root')
 );
