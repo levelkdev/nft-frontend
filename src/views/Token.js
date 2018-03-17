c98988f873fc22b1d3ddf145fec86e04d3613797
Mike Calvanese <mfcalvanese@gmail.com>
2018-03-16 23:27:13 -0400

    get rid of extra stuff

--- src/views/Token.js
+++ src/views/Token.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../../build/contracts/SimpleStorage.json'
 import getWeb3 from '../utils/getWeb3'
 import { Button } from 'react-bootstrap';
 import offers from '../img/offers.png'
@@ -7,7 +6,7 @@ import happymoogle from '../img/happy-moogle-crop.png'
 import '../css/oswald.css'
 import '../css/Token.css'
 
-class AddToken extends Component {
+class Token extends Component {
   constructor(props) {
     super(props)
 
@@ -33,56 +32,6 @@ class AddToken extends Component {
     }
 
     reader.readAsDataURL(file)
-  }
-
-  componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
-
-    getWeb3
-    .then(results => {
-      this.setState({
-        web3: results.web3
-      })
-
-      // Instantiate contract once web3 provided.
-      this.instantiateContract()
-    })
-    .catch(() => {
-      console.log('Error finding web3.')
-    })
-  }
-
-  instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
-
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
-
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
-    })
   }
 
   render() {
@@ -114,4 +63,4 @@ class AddToken extends Component {
   }
 }
 
-export default AddToken
+export default Token
