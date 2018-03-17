c9778d13e0d78f8427d56afa2862f0258c4d8f04
Gal Shenar <gshenar@gmail.com>
2018-03-17 08:43:38 -0400

    add styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+document.title = "Henko";
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
